518fd1969c105a48782a608f064b1695b50e845b
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-14 20:11:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("navbar-right").style.marginRight = "0";
   }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
