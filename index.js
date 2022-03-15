3418464cd0c765f8d98caa4983e1d1a7a3fd5b70
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-15 08:35:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
     document.getElementById("navbar-right").style.marginRight = "200px";
+    document.getElementById("logo").style.paddingLeft = "60%";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("navbar-right").style.marginRight = "0";
+    document.getElementById("logo").style.paddingLeft = "40%";
   }
