a78d11815d08b1e631d850e5889d3b2071ffbec8
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-15 08:44:58 +0500

    Update search.js

--- search.js
+++ search.js
@@ -15,12 +15,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
     document.getElementById("navbar-right").style.marginRight = "200px";
+    document.getElementById("logo").style.paddingLeft = "20%";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("navbar-right").style.marginRight = "0";
+    document.getElementById("logo").style.paddingLeft = "40%";
   }
 <!-- For search function -->
 function myFunction() {
