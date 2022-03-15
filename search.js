b4835d493ab62c2e381d6e6ecef04389c0435fe8
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-15 10:52:07 +0500

    Update search.js

--- search.js
+++ search.js
@@ -6,8 +6,8 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "70px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "30px";
   }
 }
 // for side nav 
