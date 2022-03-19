e7aa29152d1bbdb1000f5f3c2aab9853b18b605a
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-19 16:58:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementById("navbar-right").style.marginRight = "200px";
+    document.getElementById("logo").style.paddingLeft = "20%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("navbar-right").style.marginRight = "0";
+    document.getElementById("logo").style.paddingLeft = "40%";
+  }
