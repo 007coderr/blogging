a5e5615d4077f417d12ca642e99c937ab7061c47
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-19 15:35:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,17 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+//slider js
+var swiper = new Swiper('.blog-slider', {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
+    });
