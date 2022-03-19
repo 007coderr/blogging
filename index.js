a9746f3cc188e74cbd1821bcf0613ad8c9952188
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-19 15:57:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -33,16 +33,22 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 //slider js
-var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
+$(document).ready(function(){
+$(".b").click(function(){
+    $(this).toggleClass("b");
+    $(this).toggleClass("b-selected");
+ });
+});
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellalign: 'right',
+  pageDots: false,
+  groupCells: '20%',
+  selectedAttraction: 0.03,
+  friction: 0.15
+});
+var flkty = new Flickity( '.carousel', {
+  // options
+});
