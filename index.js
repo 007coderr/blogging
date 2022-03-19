fc2db69ebaba87c8ad3e181b781f89145dd4874a
007coderr <101560606+007coderr@users.noreply.github.com>
2022-03-19 17:13:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+let toggle = document.querySelector("#header .toggle-button");
+let collapse = document.querySelectorAll("#header .collapse");
+
+toggle.addEventListener('click' , function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
+})
+
+// with masonry
+new Masonry("#posts .grid", {
+    itemSelector : '.grid-item',
+    gutter : 20
+});
+
+// swiper libray initialization
+new Swiper('.swiper-container', {
+    direction : 'horizontal',
+    loop : true,
+    slidesPerView : 5,
+    autoplay : {
+        delay : 3000
+    },
+    // responsive brakpoints
+    breakpoints : {
+        '@0' : {
+            slidesPerView : 2
+        },
+        // 888px
+        '@1.00' : {
+            slidesPerView : 3
+        },
+        // 1110px
+        '@1.25' : {
+            slidesPerView : 4
+        },
+        // 1330px
+        '@1.50' : {
+            slidesPerView: 5
+        }
+    }
+})
+
+// Sticky Navigation
+window.onscroll = function(){ myFunction()};
+
+// get the current value 
+let navbar = document.getElementById("header");
+
+// get the navbar position
+let sticky = navbar.offsetTop;
+
+// sticky function
+function myFunction(){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
 }
-function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("navbar-right").style.marginRight = "200px";
-    document.getElementById("logo").style.paddingLeft = "20%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("navbar-right").style.marginRight = "0";
-    document.getElementById("logo").style.paddingLeft = "40%";
-  }
